a1e97b5247b0fa929c7e2d7fb39eb0a2458fd1c4
NULLNA <121597302@QQ.COM>
2018-02-27 09:36:42 +0800

    Update bundle.js

--- example/bundle.js
+++ example/bundle.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 import {render} from 'react-dom'
 import {Router, Route, hashHistory} from 'react-router'
 import Basic from './Basic'
-import Readonly from './Readonly'
+/*import Readonly from './Readonly'
 import Views from './Views'
 import CustomHeader from './CustomHeader'
 import CustomEventStyle from './CustomEventStyle'
@@ -14,12 +14,12 @@ import OverlapCheck from './OverlapCheck'
 import NoCrossSlotMove from './NoCrossSlotMove'
 import FreezeFirstRow from './FreezeFirstRow'
 import ResourceClickable from './ResourceClickable'
-import ComingSoon from './ComingSoon'
+import ComingSoon from './ComingSoon'*/
 
 render((
     <Router history={hashHistory}>
         <Route path="/" component={Basic}/>
-        <Route path="/readonly" component={Readonly}/>
+		{/*<Route path="/readonly" component={Readonly}/>
         <Route path="/views" component={Views}/>
         <Route path="/customheader" component={CustomHeader}/>
         <Route path="/customeventstyle" component={CustomEventStyle} />
@@ -31,7 +31,7 @@ render((
         <Route path="/nocrossslotmove" component={NoCrossSlotMove} />
         <Route path="/freezefirstrow" component={FreezeFirstRow} />
         <Route path="/resourceclickable" component={ResourceClickable} />
-        <Route path="/comingsoon" component={ComingSoon}/>
+        <Route path="/comingsoon" component={ComingSoon}/>*/}
     </Router>
-), document.getElementById('root'))
+), document.getElementById("root"))
 
